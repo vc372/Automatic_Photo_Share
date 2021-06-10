a0b29681b154698ecd634003a9459d108807b0ce
Vincent Chan <vc372@scarletmail.rutgers.edu>
2021-06-10 14:16:54 -0400

    Upload and display photo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [savedFile, setFile] = useState({uploadedFile: ""})
+  const [isFileUploaded, setIsFileUploaded] = useState(false)
+  const fileSelectedHandler = event => {
+    var uploadedFile = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      setFile(prevState => ({...prevState, uploadedFile: event.target.result}))
+    };
+    reader.readAsDataURL(uploadedFile);
+     
+    
+ 
+    
+    
+  }
+
+  const fileDisplayHandler = event => {
+    console.log(savedFile)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={fileSelectedHandler}/>
+      <button onClick={fileDisplayHandler}>Display</button>
+      <img src={savedFile.uploadedFile}/>
     </div>
   );
 }
+
+
 
 export default App;
